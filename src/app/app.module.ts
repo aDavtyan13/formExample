dc295cc832a9633385328d0d651edc122bfac9f7
aDavtyan13 <alla.davtyan99@gmail.com>
2019-10-29 03:58:55 +0400

    dam

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { FormsModule } from '@angular/forms';
 
 import { FormExampleComponent } from './form-example/form-example.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AlertModule } from 'ngx-bootstrap';
+
 
 
 
@@ -21,7 +23,8 @@ import { ReactiveFormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    AlertModule.forRoot()
     
     
   ],
--- src/app/form-example/form-example.component.ts
+++ src/app/form-example/form-example.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+import { Component, OnInit, Input } from '@angular/core';
+ import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'form-example',
@@ -7,20 +8,86 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./form-example.component.css']
 })
 export class FormExampleComponent implements OnInit {
+  myform: FormGroup;
+  username: FormControl;
+  password: FormControl;
 
   constructor() { }
 
-  myform: FormGroup;
+  karmir(){
+    
+    }
+  
 
   ngOnInit() {
+    this.createFormControls();
+    this.createForm();
+  }
+
+  createFormControls() {
+    this.username = new FormControl('', Validators.required);
+    this.password = new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+      Validators.maxLength(8)
+    ]);
+  }
+
+  createForm() {
     this.myform = new FormGroup({
-          username: new FormControl('', Validators.required),
-          password: new FormControl('', [
-            Validators.minLength(6),
-            Validators.maxLength(8),
-            Validators.required
-          ]),
-      
+      username: this.username,
+      password: this.password,
     });
   }
 }
+
+// @Component({
+//   selector: 'form-example',
+//   templateUrl: './form-example.component.html',
+//   styleUrls: ['./form-example.component.css']
+// })
+// export class FormExampleComponent implements OnInit {
+//   langs: string[] = [
+//     'English',
+//     'French',
+//     'German',
+//   ];
+//   myform: FormGroup;
+//   firstName: FormControl;
+//   lastName: FormControl;
+//   email: FormControl;
+//   password: FormControl;
+//   language: FormControl;
+
+
+//   ngOnInit() {
+//     this.createFormControls();
+//     this.createForm();
+//   }
+
+//   createFormControls() {
+//     this.firstName = new FormControl('', Validators.required);
+//     this.lastName = new FormControl('', Validators.required);
+//     this.email = new FormControl('', [
+//       Validators.required,
+//       Validators.pattern("[^ @]*@[^ @]*")
+//     ]);
+//     this.password = new FormControl('', [
+//       Validators.required,
+//       Validators.minLength(8)
+//     ]);
+//     this.language = new FormControl('');
+//   }
+
+//   createForm() {
+//     this.myform = new FormGroup({
+//       name: new FormGroup({
+//         firstName: this.firstName,
+//         lastName: this.lastName,
+//       }),
+//       email: this.email,
+//       password: this.password,
+//       language: this.language
+//     });
+//   }
+// }
