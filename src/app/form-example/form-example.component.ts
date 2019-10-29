953280a60731629d6308bc391c5d52dbeea45dbf
aDavtyan13 <alla.davtyan99@gmail.com>
2019-10-29 13:01:29 +0400

    changes

--- src/app/form-example/form-example.component.ts
+++ src/app/form-example/form-example.component.ts
@@ -14,10 +14,16 @@ export class FormExampleComponent implements OnInit {
 
   constructor() { }
 
-  karmir(){
-    
+
+    onSubmit() {
+      if (this.myform.valid) {
+        console.log("Form Submitted!");
+        this.myform.reset();
+      }
+      if (this.myform.invalid) {
+        
+      }
     }
-  
 
   ngOnInit() {
     this.createFormControls();
@@ -25,7 +31,7 @@ export class FormExampleComponent implements OnInit {
   }
 
   createFormControls() {
-    this.username = new FormControl('', Validators.required);
+    this.username = new FormControl(null, Validators.required);
     this.password = new FormControl('', [
       Validators.required,
       Validators.minLength(6),
@@ -39,55 +45,10 @@ export class FormExampleComponent implements OnInit {
       password: this.password,
     });
   }
+  
 }
 
-// @Component({
-//   selector: 'form-example',
-//   templateUrl: './form-example.component.html',
-//   styleUrls: ['./form-example.component.css']
-// })
-// export class FormExampleComponent implements OnInit {
-//   langs: string[] = [
-//     'English',
-//     'French',
-//     'German',
-//   ];
-//   myform: FormGroup;
-//   firstName: FormControl;
-//   lastName: FormControl;
-//   email: FormControl;
-//   password: FormControl;
-//   language: FormControl;
 
 
-//   ngOnInit() {
-//     this.createFormControls();
-//     this.createForm();
-//   }
 
-//   createFormControls() {
-//     this.firstName = new FormControl('', Validators.required);
-//     this.lastName = new FormControl('', Validators.required);
-//     this.email = new FormControl('', [
-//       Validators.required,
-//       Validators.pattern("[^ @]*@[^ @]*")
-//     ]);
-//     this.password = new FormControl('', [
-//       Validators.required,
-//       Validators.minLength(8)
-//     ]);
-//     this.language = new FormControl('');
-//   }
 
-//   createForm() {
-//     this.myform = new FormGroup({
-//       name: new FormGroup({
-//         firstName: this.firstName,
-//         lastName: this.lastName,
-//       }),
-//       email: this.email,
-//       password: this.password,
-//       language: this.language
-//     });
-//   }
-// }
